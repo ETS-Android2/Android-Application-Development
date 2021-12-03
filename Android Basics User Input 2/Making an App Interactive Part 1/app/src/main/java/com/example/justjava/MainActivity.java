8417cc58a7177ca434701a92ca0b8c26f2a78302
Chetan_Singh_Negi <chetansinghnegicse@gmail.com>
2021-12-03 23:58:49 +0530

    Nested Linear Layout

--- Android Basics User Input 2/Making an App Interactive Part 1/app/src/main/java/com/example/justjava/MainActivity.java
+++ Android Basics User Input 2/Making an App Interactive Part 1/app/src/main/java/com/example/justjava/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
     }
     public void decrement(View view)
     {
-        
+
         quantity = quantity-1;
         display(quantity);
     }
