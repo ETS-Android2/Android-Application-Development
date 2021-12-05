0365f86fe0ed64f088b75a5f7151382df1bc6676
Chetan_Singh_Negi <chetansinghnegicse@gmail.com>
2021-12-05 11:52:02 +0530

    Fixed minSdkVersion 30 error

--- Android Basics User Input 2/Making an App Interactive Part 1/app/src/main/java/com/example/justjava/MainActivity.java
+++ Android Basics User Input 2/Making an App Interactive Part 1/app/src/main/java/com/example/justjava/MainActivity.java
@@ -7,11 +7,11 @@
  **/
 package com.example.justjava;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.NumberFormat;
 
