0761a8c0776e96ebf9224c55a1cde6216865781e
Chetan_Singh_Negi <chetansinghnegicse@gmail.com>
2021-12-07 19:58:27 +0530

    create Method displayForTeamA & plusThreeForTeamA

--- CourtCounter/app/src/main/java/com/example/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.courtcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //void displayForTeamA();
+
+    }
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+    public void plusThreeForTheamA(View view)
+    {
+        int Score =3;
+        displayForTeamA(Score);
     }
 }
