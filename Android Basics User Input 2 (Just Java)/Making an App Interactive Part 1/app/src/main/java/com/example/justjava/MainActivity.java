3240b33bbb35d621a2df26b52a4c88ac1e5bebec
Chetan_Singh_Negi <chetansinghnegicse@gmail.com>
2021-12-08 23:15:42 +0530

    changed display method to displayQuantity

--- Android Basics User Input 2 (Just Java)/Making an App Interactive Part 1/app/src/main/java/com/example/justjava/MainActivity.java
+++ Android Basics User Input 2 (Just Java)/Making an App Interactive Part 1/app/src/main/java/com/example/justjava/MainActivity.java
@@ -40,19 +40,19 @@ public class MainActivity extends AppCompatActivity {
     {
 
         quantity =quantity+1 ;
-        display(quantity);
+        displayQuantity(quantity);
     }
     public void decrement(View view)
     {
 
         quantity = quantity-1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
