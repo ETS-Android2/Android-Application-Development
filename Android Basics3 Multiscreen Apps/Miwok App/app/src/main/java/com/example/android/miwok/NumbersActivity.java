35a78dc387521ee70a1a629534c5359b56efb4ae
Chetan_Singh_Negi <chetansinghnegicse@gmail.com>
2021-12-18 20:00:08 +0530

    add log message in NumbersActivity to view index value

--- Android Basics3 Multiscreen Apps/Miwok App/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Android Basics3 Multiscreen Apps/Miwok App/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,7 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
     @Override
@@ -34,6 +35,17 @@ public class NumbersActivity extends AppCompatActivity {
         word[0]="Eight";
         word[0]="Nine";
         word[0]="Ten";
+        Log.v("NumbersActivity","Words at index 0: "+word[0]);
+        Log.v("NumbersActivity","Words at index 1: "+word[1]);
+        Log.v("NumbersActivity","Words at index 2: "+word[2]);
+        Log.v("NumbersActivity","Words at index 3: "+word[3]);
+        Log.v("NumbersActivity","Words at index 4: "+word[4]);
+        Log.v("NumbersActivity","Words at index 5: "+word[5]);
+        Log.v("NumbersActivity","Words at index 6: "+word[6]);
+        Log.v("NumbersActivity","Words at index 7: "+word[7]);
+        Log.v("NumbersActivity","Words at index 8: "+word[8]);
+        Log.v("NumbersActivity","Words at index 9: "+word[9]);
+
 
     }
 }
