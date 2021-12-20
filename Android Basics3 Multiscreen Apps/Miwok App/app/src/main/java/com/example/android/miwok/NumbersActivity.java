27bcebc4039bc1947499cfdb47a8274481533826
Chetan_Singh_Negi <chetansinghnegicse@gmail.com>
2021-12-20 23:31:36 +0530

    Switch to Using a ListView and Adapter(udacity)

--- Android Basics3 Multiscreen Apps/Miwok App/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Android Basics3 Multiscreen Apps/Miwok App/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,8 +17,8 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -27,40 +27,36 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
-        ArrayList<String> word=new ArrayList<String>();
+        // Create a list of words
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
 
-        word.add("One");
-        word.add("Two");
-        word.add("Three");
-        word.add("Four");
-        word.add("Five");
-        word.add("Six");
-        word.add("Seven");
-        word.add("Eight");
-        word.add("Nine");
-        word.add("Ten");
-        /*Log.v("NumbersActivity","Words at index 0: "+word.get(0));
-        Log.v("NumbersActivity","Words at index 1: "+word.get(1));
-        Log.v("NumbersActivity","Words at index 2: "+word.get(2));
-        Log.v("NumbersActivity","Words at index 3: "+word.get(3));
-        Log.v("NumbersActivity","Words at index 4: "+word.get(4));
-        Log.v("NumbersActivity","Words at index 5: "+word.get(5));
-        Log.v("NumbersActivity","Words at index 6: "+word.get(6));
-        Log.v("NumbersActivity","Words at index 7: "+word.get(7));
-        Log.v("NumbersActivity","Words at index 8: "+word.get(8));
-        Log.v("NumbersActivity","Words at index 9: "+word.get(9));
+        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+        // display a single word.
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
 
-         */
-        LinearLayout root =(LinearLayout)findViewById(R.id.rootView);
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // activity_numbers.xml layout file.
+        ListView listView = (ListView) findViewById(R.id.list);
 
-
-        for(int index=0;index<word.size();index++)
-        {
-        TextView wordView=new TextView(this);
-        wordView.setText(word.get(index));
-        root.addView(wordView);
-        index +=1;
-        };
-
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+        listView.setAdapter(itemsAdapter);
     }
 }
