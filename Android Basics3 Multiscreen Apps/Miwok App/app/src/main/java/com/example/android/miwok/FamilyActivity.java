87ebc95beba286341f98c5c169b8dd498e1afe98
Chetan_Singh_Negi <chetansinghnegicse@gmail.com>
2021-12-22 20:39:47 +0530

    add list View in activity_family & add English and Miwok translations in FamilyActivity.java

--- Android Basics3 Multiscreen Apps/Miwok App/app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ Android Basics3 Multiscreen Apps/Miwok App/app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -15,8 +15,11 @@
  */
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
@@ -24,5 +27,31 @@ public class FamilyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family);
+
+        // Create a list of words
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("Father", "әpә"));
+        words.add(new Word("Mother", "әṭa"));
+        words.add(new Word("Son", "angsi"));
+        words.add(new Word("Daughter", "tune"));
+        words.add(new Word("Older Brother", "taachi"));
+        words.add(new Word("Younger Brother", "chalitti"));
+        words.add(new Word("Older Sister", "teṭe"));
+        words.add(new Word("Younger Sister", "kolliti"));
+        words.add(new Word("Grandmother", "ama"));
+        words.add(new Word("Grandfather", "paapa"));
+
+        // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
+        // adapter knows how to create list items for each item in the list.
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // activity_numbers.xml layout file.
+        ListView listView = (ListView) findViewById(R.id.familyList);
+
+        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
+        // {@link ListView} will display list items for each {@link Word} in the list.
+        listView.setAdapter(adapter);
     }
 }
