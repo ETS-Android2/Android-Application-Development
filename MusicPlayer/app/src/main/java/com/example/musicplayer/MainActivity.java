28cb274e087cec772ec09a4f968a4d8204dbb7a1
Chetan_Singh_Negi <chetansinghnegicse@gmail.com>
2022-01-01 22:07:59 +0530

    add Play and Pause Button

--- MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.musicplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button playButton =(Button)findViewById(R.id.play_button);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"play Button",Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button pauseButton =(Button)findViewById(R.id.pause_button);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Pause Button",Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
+
+
+
 }
