985c7f005bc8f8aa1190b32a93fc587bc0c03b42
Chetan_Singh_Negi <chetansinghnegicse@gmail.com>
2022-04-03 21:39:16 +0530

    comment adepter2

--- Android Basics4 Networking/QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ Android Basics4 Networking/QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -73,22 +73,22 @@ public class EarthquakeActivity extends AppCompatActivity {
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
         // Create a new {@link ArrayAdapter} of earthquakes
-        EarthquakeAdapter adapter=new EarthquakeAdapter(this,earthqakes);
+        final EarthquakeAdapter adapter=new EarthquakeAdapter(this,earthqakes);
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
         earthquakeListView.setAdapter(adapter);
 
         // Create a new adapter that takes the list of earthquakes as input
-        final EarthquakeAdapter adapter2 = new EarthquakeAdapter(this, earthqakes);
+       /* final EarthquakeAdapter adapter2 = new EarthquakeAdapter(this, earthqakes);
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
-        earthquakeListView.setAdapter(adapter2);
+        earthquakeListView.setAdapter(adapter2);*/
         earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 // Find the current earthquake that was clicked on
-                EarthquakeListItem currentEarthquake = adapter2.getItem(position);
+                EarthquakeListItem currentEarthquake = adapter.getItem(position);
 
                 // Convert the String URL into a URI object (to pass into the Intent constructor)
                 Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
