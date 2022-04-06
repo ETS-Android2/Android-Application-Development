537f8957f358db787189dbe76ffb010798e665da
Chetan_Singh_Negi <chetansinghnegicse@gmail.com>
2022-04-06 16:49:02 +0530

    Making a Preference Screen

--- Android Basics4 Networking/QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ Android Basics4 Networking/QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -25,6 +25,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -159,5 +161,20 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id =item.getItemId();
+        if(id==R.id.action_settings)
+        {
+           Intent settingIntent= new Intent(this,SettingsActivity.class);
+           startActivity(settingIntent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
