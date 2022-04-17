f102e49e649d1fe53a41856a14dcd94e9659fd62
Chetan_Singh_Negi <chetansinghnegicse@gmail.com>
2022-04-17 16:28:49 +0530

    Create PetCursorAdapter to display list of pets in ListView

--- Android Basics5 Data Storage/pets-starting-point/app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ Android Basics5 Data Storage/pets-starting-point/app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -28,6 +28,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -110,59 +111,17 @@ public class CatalogActivity extends AppCompatActivity {
             Log.v("CatalogActivity", "in else if get count");
         } else {
 
+            // Find the ListView which will be populated with the pet data
+            ListView petListView = (ListView) findViewById(R.id.list_view_pet);
 
-            TextView displayView = (TextView) findViewById(R.id.text_view_pet);
-
-            try {
-                // Create a header in the Text View that looks like this:
-                //
-                // The pets table contains <number of rows in Cursor> pets.
-                // _id - name - breed - gender - weight
-                //
-                // In the while loop below, iterate through the rows of the cursor and display
-                // the information from each column in this order.
-
-                displayView.setText("The pets table contains " + cursor.getCount() + "pets.\n\n");
-                displayView.append(PetEntry._ID + " - " +
-                        PetEntry.COLUMN_PET_NAME + " - " +
-                        PetEntry.COLUMN_PET_BREED + " - " +
-                        PetEntry.COLUMN_PET_GENDER + " - " +
-                        PetEntry.COLUMN_PET_WEIGHT + " \n ");
-
-                // Figure out the index of each column
-
-                int idColumnIndex = cursor.getColumnIndex(PetEntry._ID);
-                int nameColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_NAME);
-                int breedColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_BREED);
-                int genderColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_GENDER);
-                int weightColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_WEIGHT);
+            // Setup an Adapter to create a list item for each row of pet data in the Cursor.
+            PetCursorAdapter adapter = new PetCursorAdapter(this, cursor);
+            // Attach the adapter to the ListView.
+            petListView.setAdapter(adapter);
 
 
-                // Iterate through all the returned rows in the cursor
-                while (cursor.moveToNext()) {
-                    // Use that index to extract the String or Int value of the word
-                    // at the current row the cursor is on.
-                    int currentID = cursor.getInt(idColumnIndex);
-                    String currentName = cursor.getString(nameColumnIndex);
-                    String currentBreed = cursor.getString(breedColumnIndex);
-                    int currentGender = cursor.getInt(genderColumnIndex);
-                    int currentWeight = cursor.getInt(weightColumnIndex);
 
-                    // Display the values from each column of the current row in the cursor in the TextView
-                    displayView.append(("\n" + currentID + " - "
-                            + currentName + " - "
-                            + currentBreed + " - "
-                            + currentGender + " - "
-                            + currentWeight));
 
-                }
-
-            } finally {
-                // Always close the cursor when you're done reading from it. This releases all its
-                // resources and makes it invalid.
-
-                cursor.close();
-            }
         }
         }
 
@@ -186,7 +145,7 @@ public class CatalogActivity extends AppCompatActivity {
                     return true;
                 // Respond to a click on the "Delete all entries" menu option
                 case R.id.action_delete_all_entries:
-                    // Do nothing for now
+                    deleteAllPets();
                     return true;
             }
             return super.onOptionsItemSelected(item);
@@ -212,4 +171,12 @@ public class CatalogActivity extends AppCompatActivity {
 
 
         }
+
+    /**
+     * Helper method to delete all pets in the database.
+     */
+    private void deleteAllPets() {
+        int rowsDeleted = getContentResolver().delete(PetEntry.CONTENT_URI, null, null);
+        Log.v("CatalogActivity", rowsDeleted + " rows deleted from pet database");
+    }
     }
